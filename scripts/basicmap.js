8cdd7f9d1e9fa7711b69d14cae56cafccc1619de
Andy Gup <agup2001@msn.com>
2015-07-16 17:48:38 -0600

    Update to JS API 3.14

--- scripts/basicmap.js
+++ scripts/basicmap.js
@@ -18,14 +18,19 @@ var app = {
         //app.receivedEvent('deviceready');
         console.log('Cordova device ready event: ' + 'deviceready');
 
-        require(["esri/map"],
+        require(["esri/map", "dojo/domReady!"],
             function(Map) {
+
                 // Create map
                 var map = new Map("mapDiv",{
                     basemap: "dark-gray",
                     center: [-98.58, 39.82],
                     zoom: 3
                 });
+
+                map.on("load",function(evt) {
+                    console.log("Map loaded");
+                })
             }
         );
     }
